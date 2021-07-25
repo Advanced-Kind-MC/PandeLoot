6a53745a2b9c08266fc985dcc945702565aef859
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:09:00 -0500

    Update CommandMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/CommandMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/CommandMechanic.java
@@ -4,13 +4,13 @@ import net.seyarada.pandeloot.StringLib;
 import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 
 public class CommandMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        if (value!=null && !value.isEmpty()) {
-            StringLib.warn("++++++ Applying command effect with value "+value);
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), value);
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    if (value != null && !value.isEmpty()) {
+      StringLib.warn("++++++ Applying command effect with value " + value);
+      Bukkit.dispatchCommand((CommandSender)Bukkit.getConsoleSender(), value);
+    } 
+  }
 }
