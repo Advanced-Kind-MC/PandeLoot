4682776ab6b236c9006d9cade2a5345132c2ef1d
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:20:06 -0500

    Update BalloonScheduler.java

--- src/main/java/net/seyarada/pandeloot/schedulers/BalloonScheduler.java
+++ src/main/java/net/seyarada/pandeloot/schedulers/BalloonScheduler.java
@@ -7,25 +7,18 @@ import org.bukkit.entity.Entity;
 import org.bukkit.plugin.Plugin;
 
 public class BalloonScheduler {
-
-    private int id;
-
-    public BalloonScheduler(Entity balloon, Entity anchor) {
-
-        Plugin plugin = PandeLoot.getInstance();
-
-        id = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
-
-            if (anchor.isValid() && balloon.isValid()) {
-                balloon.teleport(anchor.getLocation().clone().add(0,4.5,0));
-                if(anchor.getFallDistance()>0) {
-                    LootBalloon.clear(balloon.getUniqueId());
-                }
-            } else {
-                Bukkit.getScheduler().cancelTask(id);
-            }
-
-        }, 0, 1);
-    }
-
+  private int id;
+  
+  public BalloonScheduler(Entity balloon, Entity anchor) {
+    PandeLoot pandeLoot = PandeLoot.getInstance();
+    this.id = Bukkit.getScheduler().scheduleSyncRepeatingTask((Plugin)pandeLoot, () -> {
+          if (anchor.isValid() && balloon.isValid()) {
+            balloon.teleport(anchor.getLocation().clone().add(0.0D, 4.5D, 0.0D));
+            if (anchor.getFallDistance() > 0.0F)
+              LootBalloon.clear(balloon.getUniqueId()); 
+          } else {
+            Bukkit.getScheduler().cancelTask(this.id);
+          } 
+        }0L, 1L);
+  }
 }
