9617f55d8e5b890f3e40cd7116feff32c442b84a
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 13:24:33 -0500

    Update MythicMobsMechanic.java

--- src/main/java/net/seyarada/pandeloot/compatibility/mythicmobs/MythicMobsMechanic.java
+++ src/main/java/net/seyarada/pandeloot/compatibility/mythicmobs/MythicMobsMechanic.java
@@ -8,6 +8,7 @@ import io.lumine.xikage.mythicmobs.skills.ITargetedEntitySkill;
 import io.lumine.xikage.mythicmobs.skills.ITargetedLocationSkill;
 import io.lumine.xikage.mythicmobs.skills.SkillMechanic;
 import io.lumine.xikage.mythicmobs.skills.SkillMetadata;
+import java.util.Collections;
 import net.seyarada.pandeloot.damage.DamageTracker;
 import net.seyarada.pandeloot.damage.DamageUtil;
 import net.seyarada.pandeloot.drops.StartDrops;
@@ -15,50 +16,37 @@ import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
-import java.util.Collections;
-
 public class MythicMobsMechanic extends SkillMechanic implements ITargetedLocationSkill, ITargetedEntitySkill {
-
-
-    public MythicMobsMechanic(MythicLineConfig config) {
-        super(config.getLine(), config);
-        this.setAsyncSafe(false);
-        this.setTargetsCreativePlayers(false);
-    }
-
-    @Override
-    public boolean castAtLocation(SkillMetadata skillMetadata, AbstractLocation abstractLocation) {
-        String i = config.getLine();
-        int j = i.indexOf("{")+1;
-        int k = i.lastIndexOf("}");
-        i = i.substring(j, k);
-
-        Location location = BukkitAdapter.adapt(abstractLocation);
-
-        new StartDrops(null, Collections.singletonList(i), null, location);
-        //new Manager().fromRewardLine(Collections.singletonList(p), Collections.singletonList(lineConfig), new DamageUtil(uuid), location);
-        return false;
-    }
-
-    @Override
-    public boolean castAtEntity(SkillMetadata skillMetadata, AbstractEntity abstractEntity) {
-        String i = config.getLine();
-        int j = i.indexOf("{")+1;
-        int k = i.lastIndexOf("}");
-        i = i.substring(j, k);
-
-        Entity entity = abstractEntity.getBukkitEntity();
-
-        DamageUtil damageUtil = null;
-        if(DamageTracker.loadedMobs.containsKey(skillMetadata.getCaster().getEntity().getUniqueId())) {
-            damageUtil = new DamageUtil(skillMetadata.getCaster().getEntity().getUniqueId());
-        }
-
-        if(entity instanceof Player) {
-            new StartDrops(Collections.singletonList((Player) entity), Collections.singletonList(i), damageUtil, entity.getLocation());
-        } else {
-            new StartDrops(null, Collections.singletonList(i), damageUtil, entity.getLocation());
-        }
-        return false;
-    }
+  public MythicMobsMechanic(MythicLineConfig config) {
+    super(config.getLine(), config);
+    setAsyncSafe(false);
+    setTargetsCreativePlayers(false);
+  }
+  
+  public boolean castAtLocation(SkillMetadata skillMetadata, AbstractLocation abstractLocation) {
+    String i = this.config.getLine();
+    int j = i.indexOf("{") + 1;
+    int k = i.lastIndexOf("}");
+    i = i.substring(j, k);
+    Location location = BukkitAdapter.adapt(abstractLocation);
+    new StartDrops(null, Collections.singletonList(i), null, location);
+    return false;
+  }
+  
+  public boolean castAtEntity(SkillMetadata skillMetadata, AbstractEntity abstractEntity) {
+    String i = this.config.getLine();
+    int j = i.indexOf("{") + 1;
+    int k = i.lastIndexOf("}");
+    i = i.substring(j, k);
+    Entity entity = abstractEntity.getBukkitEntity();
+    DamageUtil damageUtil = null;
+    if (DamageTracker.loadedMobs.containsKey(skillMetadata.getCaster().getEntity().getUniqueId()))
+      damageUtil = new DamageUtil(skillMetadata.getCaster().getEntity().getUniqueId()); 
+    if (entity instanceof Player) {
+      new StartDrops(Collections.singletonList((Player)entity), Collections.singletonList(i), damageUtil, entity.getLocation());
+    } else {
+      new StartDrops(null, Collections.singletonList(i), damageUtil, entity.getLocation());
+    } 
+    return false;
+  }
 }
