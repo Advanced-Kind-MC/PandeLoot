3e5ff8bf246a7d32a55a9de1bbe79862db79761e
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:12:31 -0500

    Update RemoveMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/RemoveMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/RemoveMechanic.java
@@ -5,12 +5,11 @@ import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 
 public class RemoveMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        StringLib.warn("++++++ Remove value is "+value);
-        if (value!=null && !value.isEmpty() && Boolean.parseBoolean(value) && reward.item!=null) {
-            StringLib.warn("++++++ Applying remove effect with value "+value);
-            reward.item.remove();
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    StringLib.warn("++++++ Remove value is " + value);
+    if (value != null && !value.isEmpty() && Boolean.parseBoolean(value) && reward.item != null) {
+      StringLib.warn("++++++ Applying remove effect with value " + value);
+      reward.item.remove();
+    } 
+  }
 }
