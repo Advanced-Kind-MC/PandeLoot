8d9d5e602b9dbdd46d3585f02cac90979d52db13
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:12:16 -0500

    Update MoneyMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/MoneyMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/MoneyMechanic.java
@@ -6,20 +6,18 @@ import net.seyarada.pandeloot.StringLib;
 import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 
 public class MoneyMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        StringLib.warn("+ Applying money effect with value "+value);
-        if (value!=null && !value.isEmpty()) {
-            final int money = Integer.parseInt(value);
-
-            if(money>0 && Bukkit.getServer().getPluginManager().getPlugin("Vault")!=null) {
-                StringLib.warn("++++++ Applying money effect with value "+value);
-                Economy economy = PandeLoot.getEconomy();
-                economy.depositPlayer(reward.player, money);
-            }
-
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    StringLib.warn("+ Applying money effect with value " + value);
+    if (value != null && !value.isEmpty()) {
+      int money = Integer.parseInt(value);
+      if (money > 0 && Bukkit.getServer().getPluginManager().getPlugin("Vault") != null) {
+        StringLib.warn("++++++ Applying money effect with value " + value);
+        Economy economy = PandeLoot.getEconomy();
+        economy.depositPlayer((OfflinePlayer)reward.player, money);
+      } 
+    } 
+  }
 }
