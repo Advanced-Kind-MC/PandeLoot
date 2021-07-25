bc620d9b2d3d4c0145c08116042422efea981061
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 13:20:01 -0500

    Update MythicMobsDrop.java

--- src/main/java/net/seyarada/pandeloot/compatibility/mythicmobs/MythicMobsDrop.java
+++ src/main/java/net/seyarada/pandeloot/compatibility/mythicmobs/MythicMobsDrop.java
@@ -8,6 +8,9 @@ import io.lumine.xikage.mythicmobs.drops.IMultiDrop;
 import io.lumine.xikage.mythicmobs.drops.LootBag;
 import io.lumine.xikage.mythicmobs.drops.droppables.ItemDrop;
 import io.lumine.xikage.mythicmobs.io.MythicLineConfig;
+import io.lumine.xikage.mythicmobs.skills.SkillCaster;
+import java.util.Collections;
+import java.util.UUID;
 import net.seyarada.pandeloot.damage.DamageTracker;
 import net.seyarada.pandeloot.damage.DamageUtil;
 import net.seyarada.pandeloot.drops.Manager;
@@ -18,39 +21,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Collections;
-import java.util.UUID;
-
 public class MythicMobsDrop extends Drop implements IMultiDrop, Listener {
-
-    private final MythicLineConfig config;
-
-    public MythicMobsDrop(MythicLineConfig config) {
-        super(config.getLine(), config);
-        this.config = config;
-    }
-
-    @Override
-    public LootBag get(DropMetadata metadata) {
-        String i = config.getLine();
-        int j = i.indexOf("{")+1;
-        int k = i.lastIndexOf("}");
-        i = i.substring(j, k);
-
-        RewardLine lineConfig = new RewardLine(i);
-        Location location = BukkitAdapter.adapt(metadata.getDropper().get().getLocation());
-        Player p = (Player) BukkitAdapter.adapt(metadata.getCause().get());
-
-        UUID uuid = metadata.getDropper().get().getEntity().getUniqueId();
-        if(DamageTracker.loadedMobs.containsKey(uuid))
-            new Manager().fromRewardLine(Collections.singletonList(p), Collections.singletonList(lineConfig), new DamageUtil(uuid), location);
-
-
-        // We don't want to drop anything with MythicMobs
-        final LootBag loot = new LootBag(metadata);
-        ItemStack item = new ItemStack(Material.AIR, 1);
-        loot.add(new ItemDrop(this.getLine(), (MythicLineConfig) this.getConfig(), new BukkitItemStack(item)));
-        return loot;
-    }
-
+  private final MythicLineConfig config;
+  
+  public MythicMobsDrop(MythicLineConfig config) {
+    super(config.getLine(), config);
+    this.config = config;
+  }
+  
+  public LootBag get(DropMetadata metadata) {
+    String i = this.config.getLine();
+    int j = i.indexOf("{") + 1;
+    int k = i.lastIndexOf("}");
+    i = i.substring(j, k);
+    RewardLine lineConfig = new RewardLine(i);
+    Location location = BukkitAdapter.adapt(((SkillCaster)metadata.getDropper().get()).getLocation());
+    Player p = (Player)BukkitAdapter.adapt(metadata.getCause().get());
+    UUID uuid = ((SkillCaster)metadata.getDropper().get()).getEntity().getUniqueId();
+    if (DamageTracker.loadedMobs.containsKey(uuid))
+      (new Manager()).fromRewardLine(Collections.singletonList(p), Collections.singletonList(lineConfig), new DamageUtil(uuid), location); 
+    LootBag loot = new LootBag(metadata);
+    ItemStack item = new ItemStack(Material.AIR, 1);
+    loot.add((Drop)new ItemDrop(getLine(), (MythicLineConfig)getConfig(), new BukkitItemStack(item)));
+    return loot;
+  }
 }
