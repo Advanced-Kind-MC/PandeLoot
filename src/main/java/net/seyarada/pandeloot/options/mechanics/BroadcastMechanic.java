dd12d54b9285de7ee104698abd936a81d40719d4
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:06:30 -0500

    Update BroadcastMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/BroadcastMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/BroadcastMechanic.java
@@ -6,11 +6,10 @@ import net.seyarada.pandeloot.rewards.Reward;
 import org.bukkit.Bukkit;
 
 public class BroadcastMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        if(value!=null && !value.isEmpty()) {
-            StringLib.warn("++++++ Applying broadcast effect with value "+value);
-            Bukkit.broadcastMessage(value);
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    if (value != null && !value.isEmpty()) {
+      StringLib.warn("++++++ Applying broadcast effect with value " + value);
+      Bukkit.broadcastMessage(value);
+    } 
+  }
 }
