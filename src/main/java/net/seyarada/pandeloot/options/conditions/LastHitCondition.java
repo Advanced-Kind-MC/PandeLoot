444e2f742032a6cc29b7cf36fdb3b06df6f8a891
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:03:30 -0500

    Update LastHitCondition.java

--- src/main/java/net/seyarada/pandeloot/options/conditions/LastHitCondition.java
+++ src/main/java/net/seyarada/pandeloot/options/conditions/LastHitCondition.java
@@ -5,13 +5,12 @@ import net.seyarada.pandeloot.rewards.Reward;
 import org.bukkit.Bukkit;
 
 public class LastHitCondition implements ConditionEvent {
-    @Override
-    public boolean onCall(Reward reward, String value) {
-        if(reward.damageUtil==null) return true;
-
-        boolean lastHit = Boolean.parseBoolean(value);
-        if(lastHit)
-            return reward.player == Bukkit.getPlayer(reward.damageUtil.lastHit);
-        return true;
-    }
+  public boolean onCall(Reward reward, String value) {
+    if (reward.damageUtil == null)
+      return true; 
+    boolean lastHit = Boolean.parseBoolean(value);
+    if (lastHit)
+      return (reward.player == Bukkit.getPlayer(reward.damageUtil.lastHit)); 
+    return true;
+  }
 }
