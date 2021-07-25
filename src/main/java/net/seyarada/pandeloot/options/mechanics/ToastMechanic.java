e61c6b5b098482ff3e79a249eb18b03f880dac91
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:13:22 -0500

    Update ToastMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/ToastMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/ToastMechanic.java
@@ -1,5 +1,6 @@
 package net.seyarada.pandeloot.options.mechanics;
 
+import java.util.Map;
 import net.seyarada.pandeloot.StringLib;
 import net.seyarada.pandeloot.nms.NMSManager;
 import net.seyarada.pandeloot.options.MechanicEvent;
@@ -7,21 +8,14 @@ import net.seyarada.pandeloot.rewards.Reward;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Map;
-
 public class ToastMechanic implements MechanicEvent {
-
-    @Override
-    public void onCall(Reward reward, String value) {
-        StringLib.warn("++++++ Applying toast effect with value "+value);
-        if(value!=null && !value.isEmpty()) {
-            final Map<String, String> options = reward.options;
-            ItemStack icon = options.get("toasticon") == null
-                    ? new ItemStack(Material.STONE, 1)
-                    : new ItemStack(Material.valueOf(options.get("toasticon").toUpperCase()), 1);
-            String frame = options.get("toastframe") == null ? "GOAL" : options.get("toastframe");
-
-            NMSManager.toast(reward.player, value, frame, icon);
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    StringLib.warn("++++++ Applying toast effect with value " + value);
+    if (value != null && !value.isEmpty()) {
+      Map<String, String> options = reward.options;
+      ItemStack icon = (options.get("toasticon") == null) ? new ItemStack(Material.STONE, 1) : new ItemStack(Material.valueOf(((String)options.get("toasticon")).toUpperCase()), 1);
+      String frame = (options.get("toastframe") == null) ? "GOAL" : options.get("toastframe");
+      NMSManager.toast(reward.player, value, frame, icon);
+    } 
+  }
 }
