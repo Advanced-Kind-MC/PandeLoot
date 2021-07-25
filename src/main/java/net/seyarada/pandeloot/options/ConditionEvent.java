8ce2a47985b796378085c3539fa3ad06c933632b
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:15:17 -0500

    Update ConditionEvent.java

--- src/main/java/net/seyarada/pandeloot/options/ConditionEvent.java
+++ src/main/java/net/seyarada/pandeloot/options/ConditionEvent.java
@@ -3,5 +3,5 @@ package net.seyarada.pandeloot.options;
 import net.seyarada.pandeloot.rewards.Reward;
 
 public interface ConditionEvent {
-    boolean onCall(Reward reward, String value);
+  boolean onCall(Reward paramReward, String paramString);
 }
