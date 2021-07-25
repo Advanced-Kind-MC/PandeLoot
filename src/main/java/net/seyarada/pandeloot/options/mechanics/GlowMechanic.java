002112524b40543f6ed3899af3d464469d6a97a9
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:10:33 -0500

    Update GlowMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/GlowMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/GlowMechanic.java
@@ -1,5 +1,6 @@
 package net.seyarada.pandeloot.options.mechanics;
 
+import java.util.Map;
 import net.Indyuce.mmoitems.ItemStats;
 import net.Indyuce.mmoitems.MMOItems;
 import net.Indyuce.mmoitems.api.Type;
@@ -8,31 +9,27 @@ import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 import net.seyarada.pandeloot.utils.ColorUtil;
 
-import java.util.Map;
-
 public class GlowMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        if (value!=null && !value.isEmpty()) {
-            StringLib.warn("++++++ Applying glow effect with value "+value);
-            final boolean shouldGlow = Boolean.parseBoolean(value);
-
-            if(shouldGlow) {
-                final Map<String, String> options = reward.options;
-                final String color;
-                if(reward.specialOptions.containsKey("matchcolor")) {
-                    String tier = MMOItems.plugin.getMMOItem(Type.get(options.get("type")), reward.rewardLine.item).getData(ItemStats.TIER).toString();
-                    StringLib.warn("+ tier: "+tier);
-                    StringLib.warn("+ pre: "+MMOItems.plugin.getTiers().getOrThrow(tier).getUnidentificationInfo().getPrefix()+"ayaya");
-                    color = ColorUtil.colorSwitch(MMOItems.plugin.getTiers().getOrThrow(tier).getUnidentificationInfo().getPrefix().substring(1));
-                    StringLib.warn("+ tier color: "+color);
-                } else color = options.get("color");
-                final double beam = Double.parseDouble(options.get("beam"));
-
-                reward.item.setGlowing(true);
-                ColorUtil.setColorEffects(reward.item, color, reward.player, beam);
-            }
-
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    if (value != null && !value.isEmpty()) {
+      StringLib.warn("++++++ Applying glow effect with value " + value);
+      boolean shouldGlow = Boolean.parseBoolean(value);
+      if (shouldGlow) {
+        String color;
+        Map<String, String> options = reward.options;
+        if (reward.specialOptions.containsKey("matchcolor")) {
+          String tier = MMOItems.plugin.getMMOItem(Type.get(options.get("type")), reward.rewardLine.item).getData(ItemStats.TIER).toString();
+          StringLib.warn("+ tier: " + tier);
+          StringLib.warn("+ pre: " + MMOItems.plugin.getTiers().getOrThrow(tier).getUnidentificationInfo().getPrefix() + "ayaya");
+          color = ColorUtil.colorSwitch(MMOItems.plugin.getTiers().getOrThrow(tier).getUnidentificationInfo().getPrefix().substring(1));
+          StringLib.warn("+ tier color: " + color);
+        } else {
+          color = options.get("color");
+        } 
+        double beam = Double.parseDouble(options.get("beam"));
+        reward.item.setGlowing(true);
+        ColorUtil.setColorEffects(reward.item, color, reward.player, beam);
+      } 
+    } 
+  }
 }
