c5d2fac2dcaa1f2021b04e5d71973ceeea2cff8b
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:10:16 -0500

    Update ExplodeMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/ExplodeMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/ExplodeMechanic.java
@@ -1,5 +1,6 @@
 package net.seyarada.pandeloot.options.mechanics;
 
+import java.util.Map;
 import net.seyarada.pandeloot.StringLib;
 import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
@@ -8,70 +9,57 @@ import org.bukkit.Location;
 import org.bukkit.entity.Item;
 import org.bukkit.util.Vector;
 
-import java.util.Map;
-
 public class ExplodeMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        if (value!=null && !value.isEmpty() && !reward.isAbandoned) {
-            StringLib.warn("++++++ Applying explode effect with value "+value);
-            final boolean shouldExplode = Boolean.parseBoolean(value);
-
-            if(shouldExplode) {
-                final Map<String, String> options = reward.options;
-                final String type = options.get("explodetype").toLowerCase();
-
-                StringLib.warn("++++++ Explode effect is type "+type);
-
-                switch (type) {
-                    default:
-                    case "spread":
-                        doSpreadDrop(reward);
-                        break;
-                    case "radial":
-                        doRadialDrop(reward);
-                        break;
-                }
-            }
-
-        }
-    }
-
-    private void doSpreadDrop(Reward reward) {
-        final Map<String, String> options = reward.options;
-        final double offset = Double.parseDouble(options.get("expoffset"));
-        final double height = Double.parseDouble(options.get("expheight"));
-
-        Vector velocity = MathUtil.getVelocity(offset, height);
-        reward.item.setVelocity(velocity);
-    }
-
-    private void doRadialDrop(Reward reward) {
-        StringLib.warn("++++++ Executing radial drop to "+reward.item);
-        final Map<String, String> options = reward.options;
-        final double radius = Double.parseDouble(options.get("exploderadius"));
-        final Item item = reward.item;
-
-        final int numberOfTotalDrops = reward.radialDropInformation.get(radius);
-        final int numberOfThisDrop = reward.radialOrder;
-
-        StringLib.warn("++++++ Total radial drops: "+numberOfTotalDrops);
-        StringLib.warn("++++++ Order of this drop: "+numberOfThisDrop);
-
-        if(numberOfTotalDrops==1) { doSpreadDrop(reward); return; }
-
-        final double angle = 2 * Math.PI / numberOfTotalDrops;
-        final double cos = Math.cos(angle * numberOfThisDrop);
-        final double sin = Math.sin(angle * numberOfThisDrop);
-        final double iX = item.getLocation().getX() + radius * cos;
-        final double iZ = item.getLocation().getZ() + radius * sin;
-
-        final Location loc = item.getLocation().clone();
-        loc.setX(iX);
-        loc.setZ(iZ);
-
-        item.setVelocity(
-                MathUtil.calculateVelocity(item.getLocation().toVector(), loc.toVector(), 0.115, 3));
-    }
-
+  public void onCall(Reward reward, String value) {
+    if (value != null && !value.isEmpty() && !reward.isAbandoned) {
+      StringLib.warn("++++++ Applying explode effect with value " + value);
+      boolean shouldExplode = Boolean.parseBoolean(value);
+      if (shouldExplode) {
+        Map<String, String> options = reward.options;
+        String type = ((String)options.get("explodetype")).toLowerCase();
+        StringLib.warn("++++++ Explode effect is type " + type);
+        switch (type) {
+          default:
+            doSpreadDrop(reward);
+            return;
+          case "radial":
+            break;
+        } 
+        doRadialDrop(reward);
+      } 
+    } 
+  }
+  
+  private void doSpreadDrop(Reward reward) {
+    Map<String, String> options = reward.options;
+    double offset = Double.parseDouble(options.get("expoffset"));
+    double height = Double.parseDouble(options.get("expheight"));
+    Vector velocity = MathUtil.getVelocity(offset, height);
+    reward.item.setVelocity(velocity);
+  }
+  
+  private void doRadialDrop(Reward reward) {
+    StringLib.warn("++++++ Executing radial drop to " + reward.item);
+    Map<String, String> options = reward.options;
+    double radius = Double.parseDouble(options.get("exploderadius"));
+    Item item = reward.item;
+    int numberOfTotalDrops = ((Integer)reward.radialDropInformation.get(Double.valueOf(radius))).intValue();
+    int numberOfThisDrop = reward.radialOrder;
+    StringLib.warn("++++++ Total radial drops: " + numberOfTotalDrops);
+    StringLib.warn("++++++ Order of this drop: " + numberOfThisDrop);
+    if (numberOfTotalDrops == 1) {
+      doSpreadDrop(reward);
+      return;
+    } 
+    double angle = 6.283185307179586D / numberOfTotalDrops;
+    double cos = Math.cos(angle * numberOfThisDrop);
+    double sin = Math.sin(angle * numberOfThisDrop);
+    double iX = item.getLocation().getX() + radius * cos;
+    double iZ = item.getLocation().getZ() + radius * sin;
+    Location loc = item.getLocation().clone();
+    loc.setX(iX);
+    loc.setZ(iZ);
+    item.setVelocity(
+        MathUtil.calculateVelocity(item.getLocation().toVector(), loc.toVector(), 0.115D, 3.0D));
+  }
 }
