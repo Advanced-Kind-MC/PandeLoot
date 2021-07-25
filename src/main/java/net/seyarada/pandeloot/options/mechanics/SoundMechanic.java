9fd487e954aab1e25a3ddbb4527bed0431ab18be
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:12:46 -0500

    Update SoundMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/SoundMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/SoundMechanic.java
@@ -5,12 +5,11 @@ import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 
 public class SoundMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        StringLib.warn("+ Test sound effect with value "+value);
-        if (value!=null && !value.isEmpty()) {
-            StringLib.warn("++++++ Applying sound effect with value "+value);
-            reward.player.playSound(reward.player.getLocation(), value, 1, 1);
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    StringLib.warn("+ Test sound effect with value " + value);
+    if (value != null && !value.isEmpty()) {
+      StringLib.warn("++++++ Applying sound effect with value " + value);
+      reward.player.playSound(reward.player.getLocation(), value, 1.0F, 1.0F);
+    } 
+  }
 }
