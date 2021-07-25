7ee1d0ae6e3becd2f33aed37ebaebec4a06aa456
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:15:33 -0500

    Update Conditions.java

--- src/main/java/net/seyarada/pandeloot/options/Conditions.java
+++ src/main/java/net/seyarada/pandeloot/options/Conditions.java
@@ -1,57 +1,51 @@
 package net.seyarada.pandeloot.options;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import net.seyarada.pandeloot.StringLib;
 import net.seyarada.pandeloot.rewards.Reward;
 
-import java.util.*;
-
 public class Conditions {
-
-    public static Map<String, ConditionEvent> listeners = new HashMap<>();
-    public static List<ConditionEvent> globalConditions = new ArrayList<>();
-
-    public static void registerOption(ConditionEvent listener, String... options) {
-        for(String option : options) {
-            option = option.toLowerCase();
-
-            listeners.put(option, listener);
-        }
-    }
-
-    public static void registerOption(ConditionEvent listener) {
-        globalConditions.add(listener);
-    }
-
-    public static void filter(List<Reward> rewards) {
-        StringLib.warn("++++ Starting reward filtering");
-        rewards.removeIf(i -> Conditions.getResult(i) && !i.skipConditions);
-    }
-
-    public static boolean getResult(Reward reward) {
-        Set<String> options = reward.options.keySet();
-
-        StringLib.warn("+++++ Checking conditions for "+reward.rewardLine.baseLine);
-
-        for(String option : options) {
-            option = option.toLowerCase();
-
-            final ConditionEvent listener = listeners.get(option);
-            if(listener==null) continue;
-
-            if(!listener.onCall(reward, reward.options.get(option)))
-                return true;
-        }
-
-        // What's this supposed to do??
-        for(ConditionEvent listener : globalConditions) {
-            if(listener==null) continue;
-
-            if(!listener.onCall(reward, null))
-                return true;
-        }
-
-        return false;
-    }
-
-
+  public static Map<String, ConditionEvent> listeners = new HashMap<>();
+  
+  public static List<ConditionEvent> globalConditions = new ArrayList<>();
+  
+  public static void registerOption(ConditionEvent listener, String... options) {
+    for (String option : options) {
+      option = option.toLowerCase();
+      listeners.put(option, listener);
+    } 
+  }
+  
+  public static void registerOption(ConditionEvent listener) {
+    globalConditions.add(listener);
+  }
+  
+  public static void filter(List<Reward> rewards) {
+    StringLib.warn("++++ Starting reward filtering");
+    rewards.removeIf(i -> (getResult(i) && !i.skipConditions));
+  }
+  
+  public static boolean getResult(Reward reward) {
+    Set<String> options = reward.options.keySet();
+    StringLib.warn("+++++ Checking conditions for " + reward.rewardLine.baseLine);
+    for (String option : options) {
+      option = option.toLowerCase();
+      ConditionEvent listener = listeners.get(option);
+      if (listener == null)
+        continue; 
+      if (!listener.onCall(reward, (String)reward.options.get(option)))
+        return true; 
+    } 
+    for (ConditionEvent listener : globalConditions) {
+      if (listener == null)
+        continue; 
+      if (!listener.onCall(reward, null))
+        return true; 
+    } 
+    return false;
+  }
 }
