6d19b4ae766fa15dc22337c44ceb58b24368cd1e
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:14:28 -0500

    Update ChanceCondition.java

--- src/main/java/net/seyarada/pandeloot/options/conditions/ChanceCondition.java
+++ src/main/java/net/seyarada/pandeloot/options/conditions/ChanceCondition.java
@@ -5,31 +5,21 @@ import net.seyarada.pandeloot.options.ConditionEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 
 public class ChanceCondition implements ConditionEvent {
-
-    boolean result;
-
-    @Override
-    public boolean onCall(Reward reward, String value) {
-
-        if(reward.options.containsKey("ignorechance")) {
-            StringLib.warn("++++++ Chance has been force ignored");
-            return true;
-        }
-
-        double chanceValue = reward.rewardLine.getChance(reward.damageUtil, reward.player);
-
-        if(chanceValue==1) return true;
-
-        StringLib.warn("++++++ Condition: Chance, Value: "+chanceValue);
-
-        double r = Math.random();
-
-        StringLib.warn("+++++++ Random generated: "+r);
-
-        result = r <= chanceValue;
-
-        StringLib.warn("++++++++ Result is: "+result);
-
-        return result;
-    }
+  boolean result;
+  
+  public boolean onCall(Reward reward, String value) {
+    if (reward.options.containsKey("ignorechance")) {
+      StringLib.warn("++++++ Chance has been force ignored");
+      return true;
+    } 
+    double chanceValue = reward.rewardLine.getChance(reward.damageUtil, reward.player);
+    if (chanceValue == 1.0D)
+      return true; 
+    StringLib.warn("++++++ Condition: Chance, Value: " + chanceValue);
+    double r = Math.random();
+    StringLib.warn("+++++++ Random generated: " + r);
+    this.result = (r <= chanceValue);
+    StringLib.warn("++++++++ Result is: " + this.result);
+    return this.result;
+  }
 }
