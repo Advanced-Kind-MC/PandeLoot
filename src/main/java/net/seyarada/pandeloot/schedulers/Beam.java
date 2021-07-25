8e62147c24fb8bb6feff453b08df530993c7d092
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:20:20 -0500

    Update Beam.java

--- src/main/java/net/seyarada/pandeloot/schedulers/Beam.java
+++ src/main/java/net/seyarada/pandeloot/schedulers/Beam.java
@@ -11,59 +11,43 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class Beam {
-
-    private int id;
-
-    // Beam effect
-
-    public Beam(Item item, String color, Location location, double height) {
-
-        String fColor = ColorUtil.getColor(item, color);
-        Color rgb = ColorUtil.getRGB(fColor);
-
-        Plugin plugin = PandeLoot.getInstance();
-
-        id = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
-
-            if (item.isValid()) {
-                if(item.isOnGround()) {
-                    double modHeight = height;
-                    while(modHeight>0) {
-                        Particle.DustOptions dustOptions = new Particle.DustOptions(rgb, 1);
-                        location.getWorld().spawnParticle(Particle.REDSTONE, item.getLocation().add(0, 0.15+modHeight, 0), 1, dustOptions);
-                        modHeight = modHeight - 0.1;
-                    }
-                }
-            } else {
-                Bukkit.getScheduler().cancelTask(id);
-            }
-
-        }, 0, 1);
-    }
-
-    public Beam(Item item, String color, Player player, double height) {
-
-        String fColor = ColorUtil.getColor(item, color);
-        Color rgb = ColorUtil.getRGB(fColor);
-
-        Plugin plugin = PandeLoot.getInstance();
-
-        id = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
-
-            if (item.isValid()) {
-                if(item.isOnGround()) {
-                    double modHeight = height;
-                    while(modHeight>0) {
-                        Particle.DustOptions dustOptions = new Particle.DustOptions(rgb, 1);
-                        player.spawnParticle(Particle.REDSTONE, item.getLocation().add(0, modHeight, 0), 1, dustOptions);
-                        modHeight = modHeight - 0.1;
-                    }
-                }
-            } else {
-                Bukkit.getScheduler().cancelTask(id);
-            }
-
-        }, 0, 1);
-    }
-
+  private int id;
+  
+  public Beam(Item item, String color, Location location, double height) {
+    String fColor = ColorUtil.getColor(item, color);
+    Color rgb = ColorUtil.getRGB(fColor);
+    PandeLoot pandeLoot = PandeLoot.getInstance();
+    this.id = Bukkit.getScheduler().scheduleSyncRepeatingTask((Plugin)pandeLoot, () -> {
+          if (item.isValid()) {
+            if (item.isOnGround()) {
+              double modHeight;
+              for (modHeight = height; modHeight > 0.0D; modHeight -= 0.1D) {
+                Particle.DustOptions dustOptions = new Particle.DustOptions(rgb, 1.0F);
+                location.getWorld().spawnParticle(Particle.REDSTONE, item.getLocation().add(0.0D, 0.15D + modHeight, 0.0D), 1, dustOptions);
+              } 
+            } 
+          } else {
+            Bukkit.getScheduler().cancelTask(this.id);
+          } 
+        }0L, 1L);
+  }
+  
+  public Beam(Item item, String color, Player player, double height) {
+    String fColor = ColorUtil.getColor(item, color);
+    Color rgb = ColorUtil.getRGB(fColor);
+    PandeLoot pandeLoot = PandeLoot.getInstance();
+    this.id = Bukkit.getScheduler().scheduleSyncRepeatingTask((Plugin)pandeLoot, () -> {
+          if (item.isValid()) {
+            if (item.isOnGround()) {
+              double modHeight;
+              for (modHeight = height; modHeight > 0.0D; modHeight -= 0.1D) {
+                Particle.DustOptions dustOptions = new Particle.DustOptions(rgb, 1.0F);
+                player.spawnParticle(Particle.REDSTONE, item.getLocation().add(0.0D, modHeight, 0.0D), 1, dustOptions);
+              } 
+            } 
+          } else {
+            Bukkit.getScheduler().cancelTask(this.id);
+          } 
+        }0L, 1L);
+  }
 }
