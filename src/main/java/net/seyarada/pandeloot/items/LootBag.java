902d3a6cb9c31a3ede3e480e1038d18f1f9258bf
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 13:41:03 -0500

    Update LootBag.java

--- src/main/java/net/seyarada/pandeloot/items/LootBag.java
+++ src/main/java/net/seyarada/pandeloot/items/LootBag.java
@@ -1,5 +1,9 @@
 package net.seyarada.pandeloot.items;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 import net.seyarada.pandeloot.Config;
 import net.seyarada.pandeloot.PandeLoot;
 import net.seyarada.pandeloot.StringLib;
@@ -15,106 +19,85 @@ import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-
-import static net.seyarada.pandeloot.items.ItemUtils.collectRewards;
-
 public class LootBag extends RewardContainer {
-
-    public static List<UUID> openedBags = new ArrayList<>();
-
-    public LootBag(String internalName, Reward reward) {
-        super(internalName, reward);
-    }
-
-    public void doGroundDrop(Player player, Item item) {
-        if(openedBags.contains(item.getUniqueId())) return;
-
-        openedBags.add(item.getUniqueId());
-
-        StringLib.warn("+ Doing ground drop for lootbag "+internalName+", from "+item.getUniqueId());
-        List<RewardLine> drops = getDrops();
-
-        StringLib.warn("+++ Got for the ground drop: ");
-        for(RewardLine drop : drops) {
-            StringLib.warn("++++ "+drop.baseLine);
-        }
-        final List<Reward> baseRewards = reward.createNewRewards(drops);
-        final List<Reward> rewards = new ArrayList<>();
-        collectRewards(baseRewards, rewards, 1, false);
-
-        Manager manager = new Manager();
-        manager.fromReward(Collections.singletonList(player), rewards, item.getLocation());
-
-        playArm(player);
-
-        int delay = manager.delay;
-
-        player.spawnParticle(Particle.FIREWORKS_SPARK, item.getLocation(), 10 ,0,0.15,0, 0.3);
-        player.spawnParticle(Particle.SMOKE_LARGE, item.getLocation(), 5 ,0,0.15,0, 0.1);
-
-        if(delay>0) {
-            item.setItemStack(NMSManager.addNBT(item.getItemStack(), StringLib.onUse, "true"));
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    item.getItemStack().setAmount(item.getItemStack().getAmount()-1);
-                    if(item.getItemStack().getAmount() <= 0) {
-                        item.remove();
-                        openedBags.remove(item.getUniqueId());
-                        return;
-                    }
-                    openedBags.remove(item.getUniqueId());
-                    item.setItemStack(NMSManager.removeNBT(item.getItemStack(), StringLib.onUse));
-                }
-            }.runTaskLater(PandeLoot.getInstance(), delay);
-        } else {
-            item.getItemStack().setAmount(item.getItemStack().getAmount()-1);
-            if(item.getItemStack().getAmount() <= 0) {
-                item.remove();
-                openedBags.remove(item.getUniqueId());
-                return;
-            }
-            openedBags.remove(item.getUniqueId());
-            item.setItemStack(NMSManager.removeNBT(item.getItemStack(), StringLib.onUse));
-        }
-    }
-
-    public ItemStack getItemStack() {
-        if(rewardContainer==null) return new ItemStack(Material.AIR, 1);
-
-        final String display = rewardContainer.getString("Display");
-        final String material = rewardContainer.getString("Material");
-        final int model = rewardContainer.getInt("Model");
-
-        ItemStack itemStack = new ItemStack(Material.valueOf(material.toUpperCase()), 1);
-        ItemMeta meta = itemStack.getItemMeta();
-        meta.setDisplayName(PlaceholderUtil.parse(display));
-        meta.setCustomModelData(model);
-        itemStack.setItemMeta(meta);
-
-        return NMSManager.addNBT(itemStack, StringLib.bag, reward.rewardLine.item);
-
-    }
-
-    private void playArm(Player player) {
-        if(player==null) return;
-
-        if(Config.getPlayArm()) {
-            if(Config.getPlayArmEmpty()) {
-                Material mainHand = player.getInventory().getItemInMainHand().getType();
-                Material offHand = player.getInventory().getItemInOffHand().getType();
-                if(mainHand==Material.AIR && offHand==Material.AIR)
-                    player.swingMainHand();
-            } else player.swingMainHand();
-
-        }
-
-    }
-
+  public static List<UUID> openedBags = new ArrayList<>();
+  
+  public LootBag(String internalName, Reward reward) {
+    super(internalName, reward);
+  }
+  
+  public void doGroundDrop(Player player, final Item item) {
+    if (openedBags.contains(item.getUniqueId()))
+      return; 
+    openedBags.add(item.getUniqueId());
+    StringLib.warn("+ Doing ground drop for lootbag " + this.internalName + ", from " + item.getUniqueId());
+    List<RewardLine> drops = getDrops();
+    StringLib.warn("+++ Got for the ground drop: ");
+    for (RewardLine drop : drops)
+      StringLib.warn("++++ " + drop.baseLine); 
+    List<Reward> baseRewards = this.reward.createNewRewards(drops);
+    List<Reward> rewards = new ArrayList<>();
+    ItemUtils.collectRewards(baseRewards, rewards, 1, false);
+    Manager manager = new Manager();
+    manager.fromReward(Collections.singletonList(player), rewards, item.getLocation());
+    playArm(player);
+    int delay = manager.delay;
+    player.spawnParticle(Particle.FIREWORKS_SPARK, item.getLocation(), 10, 0.0D, 0.15D, 0.0D, 0.3D);
+    player.spawnParticle(Particle.SMOKE_LARGE, item.getLocation(), 5, 0.0D, 0.15D, 0.0D, 0.1D);
+    if (delay > 0) {
+      item.setItemStack(NMSManager.addNBT(item.getItemStack(), "PandeLoot.onUse", "true"));
+      (new BukkitRunnable() {
+          public void run() {
+            item.getItemStack().setAmount(item.getItemStack().getAmount() - 1);
+            if (item.getItemStack().getAmount() <= 0) {
+              item.remove();
+              LootBag.openedBags.remove(item.getUniqueId());
+              return;
+            } 
+            LootBag.openedBags.remove(item.getUniqueId());
+            item.setItemStack(NMSManager.removeNBT(item.getItemStack(), "PandeLoot.onUse"));
+          }
+        }).runTaskLater((Plugin)PandeLoot.getInstance(), delay);
+    } else {
+      item.getItemStack().setAmount(item.getItemStack().getAmount() - 1);
+      if (item.getItemStack().getAmount() <= 0) {
+        item.remove();
+        openedBags.remove(item.getUniqueId());
+        return;
+      } 
+      openedBags.remove(item.getUniqueId());
+      item.setItemStack(NMSManager.removeNBT(item.getItemStack(), "PandeLoot.onUse"));
+    } 
+  }
+  
+  public ItemStack getItemStack() {
+    if (this.rewardContainer == null)
+      return new ItemStack(Material.AIR, 1); 
+    String display = this.rewardContainer.getString("Display");
+    String material = this.rewardContainer.getString("Material");
+    int model = this.rewardContainer.getInt("Model");
+    ItemStack itemStack = new ItemStack(Material.valueOf(material.toUpperCase()), 1);
+    ItemMeta meta = itemStack.getItemMeta();
+    meta.setDisplayName(PlaceholderUtil.parse(display));
+    meta.setCustomModelData(Integer.valueOf(model));
+    itemStack.setItemMeta(meta);
+    return NMSManager.addNBT(itemStack, "PandeLoot.LootBag", this.reward.rewardLine.item);
+  }
+  
+  private void playArm(Player player) {
+    if (player == null)
+      return; 
+    if (Config.getPlayArm())
+      if (Config.getPlayArmEmpty()) {
+        Material mainHand = player.getInventory().getItemInMainHand().getType();
+        Material offHand = player.getInventory().getItemInOffHand().getType();
+        if (mainHand == Material.AIR && offHand == Material.AIR)
+          player.swingMainHand(); 
+      } else {
+        player.swingMainHand();
+      }  
+  }
 }
