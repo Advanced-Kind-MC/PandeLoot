e122e0d2ed42fd62967d3b744a40bc26795475a2
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:09:26 -0500

    Update DiscordMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/DiscordMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/DiscordMechanic.java
@@ -1,25 +1,22 @@
 package net.seyarada.pandeloot.options.mechanics;
 
+import java.util.Map;
 import net.seyarada.pandeloot.StringLib;
 import net.seyarada.pandeloot.compatibility.DiscordSRVCompatibility;
 import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 
-import java.util.Map;
-
 public class DiscordMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        StringLib.warn("++++++ Applying discord effect with value "+value);
-        if(value!=null && !value.isEmpty()) {
-            final Map<String, String> options = reward.options;
-            final String title = options.get("dtitle");
-            final String message = options.get("dmessage");
-            final String color = options.get("dcolor");
-            final String link = options.get("dlink");
-            final boolean avatar = Boolean.parseBoolean(options.get("davatar"));
-
-            DiscordSRVCompatibility.embed(reward.player, title, message, value, color, link, avatar);
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    StringLib.warn("++++++ Applying discord effect with value " + value);
+    if (value != null && !value.isEmpty()) {
+      Map<String, String> options = reward.options;
+      String title = options.get("dtitle");
+      String message = options.get("dmessage");
+      String color = options.get("dcolor");
+      String link = options.get("dlink");
+      boolean avatar = Boolean.parseBoolean(options.get("davatar"));
+      DiscordSRVCompatibility.embed(reward.player, title, message, value, color, link, avatar);
+    } 
+  }
 }
