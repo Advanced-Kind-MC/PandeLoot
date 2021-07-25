486707ad954fb22a37ae0ee98cfbd2be53eaaa51
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:13:02 -0500

    Update TitleMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/TitleMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/TitleMechanic.java
@@ -1,24 +1,22 @@
 package net.seyarada.pandeloot.options.mechanics;
 
+import java.util.Map;
 import net.seyarada.pandeloot.StringLib;
 import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 
-import java.util.Map;
-
 public class TitleMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        final Map<String, String> options = reward.options;
-        final String title = options.get("title");
-        final String subtitle = options.get("subtitle");
-        final int titleFade = Integer.parseInt(options.get("titlefade"));
-        int titleDuration = Integer.parseInt(options.get("titleduration"));
-        if(titleDuration==0) titleDuration=20;
-
-        if( ( title!=null && !title.isEmpty() ) || ( subtitle!=null && !subtitle.isEmpty() ) ) {
-            StringLib.warn("++++++ Applying title effect with value "+value);
-            reward.player.sendTitle(title, subtitle, titleFade, titleDuration, titleFade);
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    Map<String, String> options = reward.options;
+    String title = options.get("title");
+    String subtitle = options.get("subtitle");
+    int titleFade = Integer.parseInt(options.get("titlefade"));
+    int titleDuration = Integer.parseInt(options.get("titleduration"));
+    if (titleDuration == 0)
+      titleDuration = 20; 
+    if ((title != null && !title.isEmpty()) || (subtitle != null && !subtitle.isEmpty())) {
+      StringLib.warn("++++++ Applying title effect with value " + value);
+      reward.player.sendTitle(title, subtitle, titleFade, titleDuration, titleFade);
+    } 
+  }
 }
