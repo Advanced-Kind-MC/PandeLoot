1c2c6054694d74a3e1c723c81273dc05a1ef4d0b
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:11:44 -0500

    Update MMOCoreExperienceMechanic.java

--- src/main/java/net/seyarada/pandeloot/options/mechanics/MMOCoreExperienceMechanic.java
+++ src/main/java/net/seyarada/pandeloot/options/mechanics/MMOCoreExperienceMechanic.java
@@ -6,15 +6,13 @@ import net.seyarada.pandeloot.options.MechanicEvent;
 import net.seyarada.pandeloot.rewards.Reward;
 
 public class MMOCoreExperienceMechanic implements MechanicEvent {
-    @Override
-    public void onCall(Reward reward, String value) {
-        if (value!=null && !value.isEmpty()) {
-            final int experience = Integer.parseInt(value);
-
-            if(experience>0) {
-                StringLib.warn("++++++ Applying mmocoreexp effect with value "+value);
-                MMOCoreCompatibility.exp(reward.player, experience);
-            }
-        }
-    }
+  public void onCall(Reward reward, String value) {
+    if (value != null && !value.isEmpty()) {
+      int experience = Integer.parseInt(value);
+      if (experience > 0) {
+        StringLib.warn("++++++ Applying mmocoreexp effect with value " + value);
+        MMOCoreCompatibility.exp(reward.player, experience);
+      } 
+    } 
+  }
 }
