27cf8c91a48f7d888dcefeffb7ce8c9912eb423e
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:24:09 -0500

    Update PandeLoot.java

--- src/main/java/net/seyarada/pandeloot/PandeLoot.java
+++ src/main/java/net/seyarada/pandeloot/PandeLoot.java
@@ -1,89 +1,77 @@
 package net.seyarada.pandeloot;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.milkbowl.vault.economy.Economy;
-import net.seyarada.pandeloot.commands.*;
+import net.seyarada.pandeloot.commands.AutoComplete;
+import net.seyarada.pandeloot.commands.BoosterCommand;
+import net.seyarada.pandeloot.commands.CommandManager;
+import net.seyarada.pandeloot.commands.GiveCommand;
+import net.seyarada.pandeloot.commands.ReloadCommand;
 import net.seyarada.pandeloot.compatibility.mythicmobs.MythicMobsCompatibility;
 import net.seyarada.pandeloot.damage.DamageTracker;
 import net.seyarada.pandeloot.items.LootBalloon;
 import net.seyarada.pandeloot.nms.PlayerListener;
 import net.seyarada.pandeloot.options.RegisterOptions;
 import net.seyarada.pandeloot.rewards.RewardsListener;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class PandeLoot extends JavaPlugin {
-
-    private static PandeLoot instance;
-    private static Economy econ = null;
-    public static final List<ArmorStand> totalHolograms = new ArrayList<>();
-
-    @Override
-    public void onEnable() {
-
-        instance = this;
-
-        new Config();
-        if(getServer().getPluginManager().getPlugin("MythicMobs")!=null) {
-            MythicMobsCompatibility mmComp = new MythicMobsCompatibility();
-            this.getServer().getPluginManager().registerEvents(mmComp, this);
-        }
-
-        new RegisterOptions();
-
-        this.getServer().getPluginManager().registerEvents(new DamageTracker(), this);
-        this.getServer().getPluginManager().registerEvents(new RewardsListener(), this);
-        this.getServer().getPluginManager().registerEvents(new PlayerListener(), this);
-
-        this.getServer().getPluginManager().registerEvents(new LootBalloon(null, null), this);
-
-        this.getCommand("pandeloot").setExecutor(new CommandManager());
-
-        // Register auto completion
-
-        new BoosterCommand();
-        new GiveCommand();
-        new ReloadCommand();
-
-        this.getCommand("pandeloot").setTabCompleter(new AutoComplete());
-
-        setupEconomy();
-
-    }
-
-    @Override
-    public void onDisable() {
-        // Removes possible holograms that may be alive at the time of reload, so they don't get stuck
-        // in a "frozen" state.
-        try {
-            for(ArmorStand i : totalHolograms) {
-                if(i!=null && i.isValid()) {
-                    i.remove();
-                }
-            }
-        } catch (Exception ignored) { }
-    }
-
-    private void setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return;
-        }
-        econ = rsp.getProvider();
-    }
-
-    public static Economy getEconomy() {
-        return econ;
-    }
-
-    public static PandeLoot getInstance() {
-        return instance;
-    }
-
+  private static PandeLoot instance;
+  
+  private static Economy econ = null;
+  
+  public static final List<ArmorStand> totalHolograms = new ArrayList<>();
+  
+  public void onEnable() {
+    instance = this;
+    new Config();
+    if (getServer().getPluginManager().getPlugin("MythicMobs") != null) {
+      MythicMobsCompatibility mmComp = new MythicMobsCompatibility();
+      getServer().getPluginManager().registerEvents((Listener)mmComp, (Plugin)this);
+    } 
+    new RegisterOptions();
+    getServer().getPluginManager().registerEvents((Listener)new DamageTracker(), (Plugin)this);
+    getServer().getPluginManager().registerEvents((Listener)new RewardsListener(), (Plugin)this);
+    getServer().getPluginManager().registerEvents((Listener)new PlayerListener(), (Plugin)this);
+    getServer().getPluginManager().registerEvents((Listener)new LootBalloon(null, null), (Plugin)this);
+    getCommand("pandeloot").setExecutor((CommandExecutor)new CommandManager());
+    new BoosterCommand();
+    new GiveCommand();
+    new ReloadCommand();
+    getCommand("pandeloot").setTabCompleter((TabCompleter)new AutoComplete());
+    setupEconomy();
+  }
+  
+  public void onDisable() {
+    try {
+      for (ArmorStand i : totalHolograms) {
+        if (i != null && i.isValid())
+          i.remove(); 
+      } 
+    } catch (Exception exception) {}
+  }
+  
+  private void setupEconomy() {
+    if (getServer().getPluginManager().getPlugin("Vault") == null)
+      return; 
+    RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+    if (rsp == null)
+      return; 
+    econ = (Economy)rsp.getProvider();
+  }
+  
+  public static Economy getEconomy() {
+    return econ;
+  }
+  
+  public static PandeLoot getInstance() {
+    return instance;
+  }
 }
