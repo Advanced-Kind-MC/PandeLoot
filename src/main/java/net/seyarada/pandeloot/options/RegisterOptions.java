7522c6f7a0ea5b93ec1f3a87fb5fe796ed2844b3
IAmSoccer <62361314+IAmSoccer@users.noreply.github.com>
2021-07-25 14:16:45 -0500

    Update RegisterOptions.java

--- src/main/java/net/seyarada/pandeloot/options/RegisterOptions.java
+++ src/main/java/net/seyarada/pandeloot/options/RegisterOptions.java
@@ -1,43 +1,63 @@
 package net.seyarada.pandeloot.options;
 
-import net.seyarada.pandeloot.options.conditions.*;
-import net.seyarada.pandeloot.options.mechanics.*;
+import net.seyarada.pandeloot.options.conditions.ChanceCondition;
+import net.seyarada.pandeloot.options.conditions.DamageCondition;
+import net.seyarada.pandeloot.options.conditions.LastHitCondition;
+import net.seyarada.pandeloot.options.conditions.PermissionBlacklistCondition;
+import net.seyarada.pandeloot.options.conditions.PermissionCondition;
+import net.seyarada.pandeloot.options.conditions.TopCondition;
+import net.seyarada.pandeloot.options.mechanics.ActionbarMechanic;
+import net.seyarada.pandeloot.options.mechanics.BroadcastMechanic;
+import net.seyarada.pandeloot.options.mechanics.CommandMechanic;
+import net.seyarada.pandeloot.options.mechanics.DiscordMechanic;
+import net.seyarada.pandeloot.options.mechanics.ExperienceMechanic;
+import net.seyarada.pandeloot.options.mechanics.ExplodeMechanic;
+import net.seyarada.pandeloot.options.mechanics.GlowMechanic;
+import net.seyarada.pandeloot.options.mechanics.HolobroadcastMechanic;
+import net.seyarada.pandeloot.options.mechanics.HologramMechanic;
+import net.seyarada.pandeloot.options.mechanics.MMOCoreExperienceMechanic;
+import net.seyarada.pandeloot.options.mechanics.MessageMechanic;
+import net.seyarada.pandeloot.options.mechanics.MoneyMechanic;
+import net.seyarada.pandeloot.options.mechanics.RemoveMechanic;
+import net.seyarada.pandeloot.options.mechanics.SoundMechanic;
+import net.seyarada.pandeloot.options.mechanics.TitleMechanic;
+import net.seyarada.pandeloot.options.mechanics.ToastMechanic;
+import net.seyarada.pandeloot.options.mechanics.TotemMechanic;
+import net.seyarada.pandeloot.options.mechanics.VisibilityMechanic;
 
 public class RegisterOptions {
-
-    public RegisterOptions() {
-        registerConditions();
-        registerMechanics();
-    }
-
-    private void registerConditions() {
-        Conditions.registerOption(new ChanceCondition());
-        Conditions.registerOption(new DamageCondition());
-        Conditions.registerOption(new LastHitCondition());
-        Conditions.registerOption(new PermissionBlacklistCondition(), "permissionblacklist", "pbl");
-        Conditions.registerOption(new PermissionCondition(), "permission");
-        Conditions.registerOption(new TopCondition(), "top");
-    }
-
-    private void registerMechanics() {
-        Options.registerOption(new ActionbarMechanic(), OptionType.PLAYER, "actionbar");
-        Options.registerOption(new BroadcastMechanic(), OptionType.GENERAL, "broadcast");
-        Options.registerOption(new CommandMechanic(), OptionType.GENERAL, "command", "cmd");
-        Options.registerOption(new DiscordMechanic(), OptionType.GENERAL, "dchannel");
-        Options.registerOption(new ExperienceMechanic(), OptionType.PLAYER, "experience", "exp", "xp");
-        Options.registerOption(new ExplodeMechanic(), OptionType.ITEM, "explode");
-        Options.registerOption(new GlowMechanic(), OptionType.ITEM, "glow");
-        Options.registerOption(new HolobroadcastMechanic(), OptionType.PLAYER, "holobroadcast", "hb");
-        Options.registerOption(new HologramMechanic(), OptionType.ITEM, "hologram");
-        Options.registerOption(new MessageMechanic(), OptionType.PLAYER, "message", "msg");
-        Options.registerOption(new MMOCoreExperienceMechanic(), OptionType.PLAYER, "mmocexp", "coreexp");
-        Options.registerOption(new MoneyMechanic(), OptionType.PLAYER, "money", "eco");
-        Options.registerOption(new RemoveMechanic(), OptionType.GENERAL, "remove");
-        Options.registerOption(new SoundMechanic(), OptionType.PLAYER, "sound");
-        Options.registerOption(new TitleMechanic(), OptionType.PLAYER, "title", "subtitle");
-        Options.registerOption(new ToastMechanic(), OptionType.PLAYER, "toast", "toasttitle");
-        Options.registerOption(new TotemMechanic(), OptionType.PLAYER, "totemeffect", "playtotem", "totem");
-        Options.registerOption(new VisibilityMechanic(), OptionType.ITEM, "visibility", "canview");
-    }
-
+  public RegisterOptions() {
+    registerConditions();
+    registerMechanics();
+  }
+  
+  private void registerConditions() {
+    Conditions.registerOption((ConditionEvent)new ChanceCondition());
+    Conditions.registerOption((ConditionEvent)new DamageCondition());
+    Conditions.registerOption((ConditionEvent)new LastHitCondition());
+    Conditions.registerOption((ConditionEvent)new PermissionBlacklistCondition(), new String[] { "permissionblacklist", "pbl" });
+    Conditions.registerOption((ConditionEvent)new PermissionCondition(), new String[] { "permission" });
+    Conditions.registerOption((ConditionEvent)new TopCondition(), new String[] { "top" });
+  }
+  
+  private void registerMechanics() {
+    Options.registerOption((MechanicEvent)new ActionbarMechanic(), OptionType.PLAYER, new String[] { "actionbar" });
+    Options.registerOption((MechanicEvent)new BroadcastMechanic(), OptionType.GENERAL, new String[] { "broadcast" });
+    Options.registerOption((MechanicEvent)new CommandMechanic(), OptionType.GENERAL, new String[] { "command", "cmd" });
+    Options.registerOption((MechanicEvent)new DiscordMechanic(), OptionType.GENERAL, new String[] { "dchannel" });
+    Options.registerOption((MechanicEvent)new ExperienceMechanic(), OptionType.PLAYER, new String[] { "experience", "exp", "xp" });
+    Options.registerOption((MechanicEvent)new ExplodeMechanic(), OptionType.ITEM, new String[] { "explode" });
+    Options.registerOption((MechanicEvent)new GlowMechanic(), OptionType.ITEM, new String[] { "glow" });
+    Options.registerOption((MechanicEvent)new HolobroadcastMechanic(), OptionType.PLAYER, new String[] { "holobroadcast", "hb" });
+    Options.registerOption((MechanicEvent)new HologramMechanic(), OptionType.ITEM, new String[] { "hologram" });
+    Options.registerOption((MechanicEvent)new MessageMechanic(), OptionType.PLAYER, new String[] { "message", "msg" });
+    Options.registerOption((MechanicEvent)new MMOCoreExperienceMechanic(), OptionType.PLAYER, new String[] { "mmocexp", "coreexp" });
+    Options.registerOption((MechanicEvent)new MoneyMechanic(), OptionType.PLAYER, new String[] { "money", "eco" });
+    Options.registerOption((MechanicEvent)new RemoveMechanic(), OptionType.GENERAL, new String[] { "remove" });
+    Options.registerOption((MechanicEvent)new SoundMechanic(), OptionType.PLAYER, new String[] { "sound" });
+    Options.registerOption((MechanicEvent)new TitleMechanic(), OptionType.PLAYER, new String[] { "title", "subtitle" });
+    Options.registerOption((MechanicEvent)new ToastMechanic(), OptionType.PLAYER, new String[] { "toast", "toasttitle" });
+    Options.registerOption((MechanicEvent)new TotemMechanic(), OptionType.PLAYER, new String[] { "totemeffect", "playtotem", "totem" });
+    Options.registerOption((MechanicEvent)new VisibilityMechanic(), OptionType.ITEM, new String[] { "visibility", "canview" });
+  }
 }
