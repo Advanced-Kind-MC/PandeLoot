c0d88b05d10f220f5bbc481369d9f43c95feaec2
Hugo B <hugo.bayer@aon.at>
2021-08-08 19:50:13 +0200

    loot items no longer die, who wants that anyway?

--- src/main/java/net/seyarada/pandeloot/nms/PlayerListener.java
+++ src/main/java/net/seyarada/pandeloot/nms/PlayerListener.java
@@ -1,26 +1,36 @@
 package net.seyarada.pandeloot.nms;
 
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
 
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent e) {
-		NMSManager.injectPlayer(e.getPlayer());
-	}
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        NMSManager.injectPlayer(e.getPlayer());
+    }
 
-	@EventHandler
-	public void onPlayerQuit(PlayerQuitEvent e) {
-		NMSManager.removePlayer(e.getPlayer());
-	}
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        NMSManager.removePlayer(e.getPlayer());
+    }
 
-	@EventHandler
-	public void onItemRemove(EntityPickupItemEvent e) {
-		NMSManager.hideItemFromPlayerMap.remove(e.getItem().getEntityId());
-	}
+    @EventHandler
+    public void onItemRemove(EntityPickupItemEvent e) {
+        NMSManager.hideItemFromPlayerMap.remove(e.getItem().getEntityId());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.NORMAL)
+    public void onItemDeath(EntityDamageEvent e) {
+        if (e.getEntity() instanceof Item && NMSManager.hideItemFromPlayerMap.containsKey(e.getEntity().getEntityId())) {
+            e.setCancelled(true);
+        }
+    }
 
 }
